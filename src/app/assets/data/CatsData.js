dc358960401ffb75e332872972d9dbefac821db2
SnezzyDwarf <bruno.pernao@essp.pt>
2026-04-13 19:54:28 +0100

    Insert photo cats gallery

--- src/app/assets/data/CatsData.js
+++ src/app/assets/data/CatsData.js
@@ -20,6 +20,56 @@ const slides = [
     after: "/cats/Freddy/FreddyAfter.jpeg",
     before: "/cats/Freddy/FreddyBefore.jpeg",
   },
+  {
+    name: "Jack",
+    after: "/cats/Jack/JackAfter.jpeg",
+    before: "/cats/Jack/JackBefore.jpeg",
+  },
+  {
+    name: "Julieta",
+    after: "/cats/Julieta/JulietaAfter.jpeg",
+    before: "/cats/Julieta/JulietaBefore.jpeg",
+  },
+  {
+    name: "Mochi",
+    after: "/cats/Mochi/MochiAfter.jpeg",
+    before: "/cats/Mochi/MochiBefore.jpeg",
+  },
+  {
+    name: "Mochi",
+    after: "/cats/Mochi2/Mochi2After.jpeg",
+    before: "/cats/Mochi2/Mochi2bEFORE.jpeg",
+  },
+  {
+    name: "Molly",
+    after: "/cats/Molly/MollyAfter.jpeg",
+    before: "/cats/Molly/MollyBefore.jpeg",
+  },
+  {
+    name: "Noa&Luna",
+    after: "/cats/Noa&Luna/Noa&LunaAfter.jpeg",
+    before: "/cats/Noa&Luna/Noa&LunaBefore.jpeg",
+  },
+  {
+    name: "Sharlotka",
+    after: "/cats/Sharlotka/SharlAfter.jpeg",
+    before: "/cats/Sharlotka/SharlBefore.jpeg",
+  },
+  {
+    name: "Simba",
+    after: "/cats/Simba/SimbaAfter.jpeg",
+    before: "/cats/Simba/SimbaBefore.jpeg",
+  },
+  {
+    name: "Sorte",
+    after: "/cats/Sorte/SorteAfter.jpeg",
+    before: "/cats/Sorte/SorteBefore.jpeg",
+  },
+  {
+    name: "Vitória",
+    after: "/cats/Vitoria/VitoriaAfter.jpeg",
+    before: "/cats/Vitoria/VitoriaBefore.jpeg",
+  },
 ];
 
 export default slides;
